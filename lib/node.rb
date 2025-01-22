849f7b6a7a8a0589b303659652d94b7d8c274682
TunaSandwinch <winch.mascarenas@gmail.com>
2025-01-22 20:28:26 +0800

    feat: add prev_node instance variable

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,11 @@
-class Node
-  attr_accessor :value, :next_node
-  def initialize(value, next_node = nil)
+# frozen_string_literal: true
+
+class Node # rubocop:disable Style/Documentation
+  attr_accessor :value, :next_node, :prev_node
+
+  def initialize(value, prev_node = nil, next_node = nil)
     @value = value
+    @prev_node = prev_node
     @next_node = next_node
   end
 end
